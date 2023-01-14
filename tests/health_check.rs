72b590e229c1489133035824351a6aea09df7e5e
jason <ja.schultz@gmail.com>
2023-01-13 23:36:37 -0700

    [6.0] Email Validation
    Created new domain folder to hold subscriber and name/email files
    Added faker crate to help with testing
    Add validations for email address
    Added tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 //! tests/health_check.rs
+use actix_web::rt::spawn;
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute. //
 // You can inspect what code gets generated using
@@ -52,6 +53,37 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 
     clean_up_database(&app.db_settings).await;
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 Bad Request when the payload was {}.",
+            description
+        );
+    }
 }
 
 #[tokio::test]
